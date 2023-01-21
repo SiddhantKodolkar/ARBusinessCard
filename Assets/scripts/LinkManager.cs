d7050487689d15f4c14f06cfc64d24db019bd191
Siddhant Kodolkar <kodolkar.siddhant19@gmail.com>
2023-01-21 11:07:18 +0530

    Initial Commit

---
+++ Assets/scripts/LinkManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void openLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/siddhant-kodolkar-927505203/");
+    }
+    
+    public void openGithub()
+    {
+        Application.OpenURL("https://github.com/SiddhantKodolkar");
+    }
+    
+    public void openLeetcode()
+    {
+        Application.OpenURL("https://leetcode.com/SiddhantKodolkar/");
+    }
+    public void openCodingNinja()
+    {
+        Application.OpenURL("https://www.codingninjas.com/codestudio/profile/b2c62ef8-3669-4bb7-ac58-22c0afc2ac9f");
+    }
+    public void openPortfolio()
+    {
+        Application.OpenURL("https://siddhantkodolkar.github.io/");
+    }
+
+
+}
